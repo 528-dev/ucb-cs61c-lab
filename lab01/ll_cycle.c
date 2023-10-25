fc493f4c5cc93126413753e3dc4551afeff0ed15
ljh <508328509@qq.com>
2023-10-25 11:00:53 +0000

    lab01 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,13 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *p, *q;
+    p = q = head;
+    while (q){
+        if (q->next) q = q->next->next;
+        else return 0;
+        p = p->next;
+        if (p == q) return 1;
+    }
     return 0;
 }
